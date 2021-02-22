641f706d7c0c8bfd5307ed59d541a21ed398ae5a
treeplate <tree.plate.moo@gmail.com>
2021-02-21 17:39:18 -0800

    add object and goal support

--- lib/main.dart
+++ lib/main.dart
@@ -42,30 +42,39 @@ class _MyHomePageState extends State<MyHomePage> {
   _MyHomePageState() {
     final PacketBuffer buffer = PacketBuffer();
     server.add([playerIndex, 0]);
-    server.listen((List<int> imessage) {
-      print("packet $imessage");
-      buffer.add(imessage as Uint8List);
-      if (buffer.available > 7) {
-        print("avail: ${buffer.available}.");
-        int size = buffer.readInt64();
-        buffer.checkpoint();
-        print("avail: ${buffer.available} (size $size).");
-        while (buffer.available >= size) {
-          print("avail: ${buffer.available}.");
-          message = buffer.readUint8List(size);
-          print("messag" + message.toString());
-          buffer.checkpoint();
+    server.listen((List<int> message) {
+      buffer.add(message as Uint8List);
+      if (buffer.available >= 16) {
+        int playerCount = buffer.readInt64();
+        int objectCount = buffer.readInt64();
+        if (buffer.available >= 2 + playerCount * 2 + objectCount * 2 + 2) {
+          setState(() {
+            size = buffer.readUint8List(2);
+            rawPlayers = buffer.readUint8List(playerCount * 2);
+            rawObjects = buffer.readUint8List(playerCount * 2);
+            goal = buffer.readUint8List(2);
+          });
         }
       }
-      setState(() {});
     });
   }
 
-  List<int> message = [0, 0];
+  List<int> goal = [20, 20];
+  List<int> size = [0, 0];
+
+  List<int> rawPlayers = [];
   List<Offset> get players {
-    print("msg" + message.toString());
-    List<int> poses = message.sublist(2, message.length);
-    print("poses: " + poses.toString());
+    List<int> poses = rawPlayers;
+    List<Offset> result = [];
+    for (int i = 0; i < poses.length; i += 2) {
+      result.add(Offset(poses[i] / 1, poses[i + 1] / 1));
+    }
+    return result;
+  }
+
+  List<int> rawObjects = [];
+  List<Offset> get objects {
+    List<int> poses = rawObjects;
     List<Offset> result = [];
     for (int i = 0; i < poses.length; i += 2) {
       result.add(Offset(poses[i] / 1, poses[i + 1] / 1));
@@ -99,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   TextButton(
                     onPressed: () {
-                      if(playerIndex>0) {
+                      if (playerIndex > 0) {
                         playerIndex--;
                         server.add([playerIndex, 0]);
                       }
@@ -112,8 +121,8 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Container(
               color: Colors.black,
               child: CustomPaint(
-                painter:
-                    WorldDrawer(Size(message[0] / 1, message[1] / 1), players),
+                painter: WorldDrawer(
+                    Size(size[0] / 1, size[1] / 1), players, objects, goal),
                 child: SizedBox.expand(),
               ),
             ),
@@ -126,10 +135,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class WorldDrawer extends CustomPainter {
   final List<Offset> players;
+  final List<Offset> objects;
+  final List<int> goal;
 
   final Size size;
 
-  WorldDrawer(this.size, this.players);
+  WorldDrawer(this.size, this.players, this.objects, this.goal);
 
   @override
   void paint(Canvas canvas, Size totalSize) {
@@ -160,6 +171,17 @@ class WorldDrawer extends CustomPainter {
             ..color = Color.fromARGB(
                 0xFF, r.nextInt(0xFF), r.nextInt(0xFF), r.nextInt(0xFF)));
     }
+    for (int i = 0; i < objects.length; i++) {
+      Offset offset = objects[i];
+      canvas.drawRect(
+        topLeft + (offset * circleRadius * 2) & Size.square(circleRadius * 2),
+        Paint()..color = Colors.black,
+      );
+    }
+    canvas.drawRect(
+        topLeft + (Offset(goal[0]/1, goal[1]/1) * circleRadius * 2) & Size.square(circleRadius * 2),
+        Paint()..color = Colors.lightGreen,
+      );
   }
 
   @override
