ce91f145d00f6406a7f2ad7e631f1d41787207ea
treeplate <tree.plate.moo@gmail.com>
2021-02-21 17:45:53 -0800

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           setState(() {
             size = buffer.readUint8List(2);
             rawPlayers = buffer.readUint8List(playerCount * 2);
-            rawObjects = buffer.readUint8List(playerCount * 2);
+            rawObjects = buffer.readUint8List(objectCount * 2);
             goal = buffer.readUint8List(2);
           });
         }
