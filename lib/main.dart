4b2924f1f81a4a2acdfc8de9e7dce20aa9d5a095
treeplate <tree.plate.moo@gmail.com>
2021-03-20 15:43:00 -0700

    large changes?

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,8 +17,91 @@ List<String> levelNames = [
   "movement_intro",
   "button_intro",
   "double_button",
-  /*"box_intro"*/ "button_intro",
+  "box_intro",
+  "box_puzzle",
 ];
+
+class WorldState {
+  WorldState(this.size, this.players, this.objects, this.goal);
+  final Size size; // fixed per level
+  final List<Offset> players;
+  final List<List<double>> objects;
+  final List<int> goal; // fixed per level
+}
+
+bool playerMoving = false;
+
+class WorldTween extends Tween<WorldState> {
+  WorldTween(WorldState begin, WorldState end) : super(begin: begin, end: end);
+  WorldState lerp(double t) {
+    List<List<double>> objects = [];
+    for (List<double> objectA in begin.objects) {
+      if (!end.objects.any((List<double> tester) => tester[4] == objectA[4])) {
+        objects.add(objectA);
+        continue;
+      }
+      print("Animating $objectA");
+      List<double> objectB = end.objects
+          .singleWhere((List<double> tester) => tester[4] == objectA[4]);
+      switch (objectB[2].toInt()) {
+        case 0:
+        case 1:
+          print("lerping 0/1...");
+          objects.add([
+            lerpDouble(objectA[0], objectB[0], t),
+            lerpDouble(objectA[1], objectB[1], t),
+            objectB[2],
+            0,
+            objectB[4],
+          ]);
+          break;
+        case 2:
+          print("lerping 2... (${lerpDouble(objectA[3], objectB[3], t)})");
+          objects.add([
+            objectB[0],
+            objectB[1],
+            2,
+            lerpDouble(objectA[3], objectB[3], t),
+            objectB[4],
+          ]);
+          break;
+        case 3:
+          print(
+              "lerping 3... (${lerpDouble(objectA[0], objectB[0], t)}, ${lerpDouble(objectA[1], objectB[1], t)},");
+          objects.add([
+            lerpDouble(objectA[0], objectB[0], t),
+            lerpDouble(objectA[1], objectB[1], t),
+            3,
+            0,
+            objectB[4]
+          ]);
+          break;
+        case 4:
+          print("lerping 4...");
+          objects.add(objectB);
+          break;
+        default:
+          throw "Unreckognized object type ${objectB[3]}";
+      }
+    }
+    if(begin.players.length > playerIndex) print("${begin.players[playerIndex]} !=/== ${end.players[playerIndex]}");
+    playerMoving = (begin.players.length > playerIndex) && begin.players[playerIndex] != end.players[playerIndex];
+    return WorldState(
+      end.size,
+      begin.players
+          .map(
+            (Offset x) => Offset.lerp(
+              x,
+              end.players[begin.players.indexOf(x)],
+              t,
+            ),
+          )
+          .toList(),
+      objects,
+      end.goal,
+    );
+  }
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,7 +116,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool playing = false;
+  bool playingGame = false;
+  bool muted = false;
 
   @override
   Widget build(BuildContext context) {
@@ -41,10 +126,20 @@ class _MyAppState extends State<MyApp> {
           <T>(RouteSettings settings, Widget Function(BuildContext) func) =>
               MaterialPageRoute<T>(builder: func),
       title: 'Flutter Demo',
-      color: Colors.black,
-      home: playing
-          ? GameScreen(title: 'Flutter Demo Home Page')
-          : TitleScreen(() => setState(() => playing = true)),
+      color: Colors.brown,
+      home: Stack(
+        children: [
+          Material(
+            child: IconButton(
+              icon: Icon(muted ? Icons.volume_off : Icons.volume_up),
+              onPressed: () => setState(() => muted = !muted),
+            ),
+          ),
+          playingGame
+              ? GameScreen(title: 'Flutter Demo Home Page', muted: muted)
+              : TitleScreen(() => setState(() => playingGame = true)),
+        ],
+      ),
     );
   }
 }
@@ -71,12 +166,15 @@ class TitleScreen extends StatelessWidget {
             height: 100,
           ),
           painter: WorldDrawer(
-            Size.square(1),
-            [],
-            [
-              [0, 0, 2, 0],
-            ],
-            [20, 20],
+            WorldState(
+              Size.square(1),
+              [Offset(20, 20)],
+              [
+                [0, 0, 2, 0],
+              ],
+              [20, 20],
+            ),
+            null,
             null,
           ),
         ),
@@ -92,16 +190,18 @@ class TitleScreen extends StatelessWidget {
 }
 
 class GameScreen extends StatefulWidget {
-  GameScreen({Key /*?*/ key, this.title}) : super(key: key);
+  GameScreen({Key /*?*/ key, this.title, this.muted}) : super(key: key);
 
   final String title;
+  final bool muted;
 
   @override
   _GameScreenState createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
-  ImageInfo _imageInfo;
+  ImageInfo _buttonImageInfo;
+  ImageInfo _crateImageInfo;
   void initState() {
     super.initState();
     ExactAssetImage('pixelart/button.png')
@@ -109,42 +209,50 @@ class _GameScreenState extends State<GameScreen> {
         .addListener(
       ImageStreamListener((ImageInfo imageInfo, bool syncronous) {
         setState(() {
-          _imageInfo = imageInfo;
+          _buttonImageInfo = imageInfo;
+        });
+      }),
+    );
+    ExactAssetImage('pixelart/crate.png')
+        .resolve(ImageConfiguration(bundle: rootBundle))
+        .addListener(
+      ImageStreamListener((ImageInfo imageInfo, bool syncronous) {
+        setState(() {
+          _crateImageInfo = imageInfo;
         });
       }),
     );
     final PacketBuffer buffer = PacketBuffer();
     server.add([playerIndex, 0]);
     server.listen((List<int> message) {
-      print("got message $message");
+      //print("got message $message");
       buffer.add(message as Uint8List);
       if (buffer.available >= 16) {
         int playerCount = buffer.readInt64();
         int objectCount = buffer.readInt64();
-        print(
-            "(${buffer.available}) hopefully >= 3 + $playerCount * 2 + $objectCount * 4 + 2");
+        print("(${buffer.available}) hopefully >= 3 + $playerCount * 2 + $objectCount * 4 + 2");
         buffer.rewind();
         if (buffer.available >=
-            16 + 3 + playerCount * 2 + objectCount * 4 + 2) {
-          print("Yes, it is.");
+            16 + 3 + playerCount * 2 + objectCount * 5 + 2) {
+          //print("Yes, it is.");
           buffer.readUint8List(16);
           setState(() {
             List<int> someData = buffer.readUint8List(3);
             size = someData.sublist(0, 2);
             if (lastLevelPlayed != someData[2]) {
               lastLevelPlayed = someData[2];
-              String filename =
-                  "audio/${levelNames[lastLevelPlayed - 1]}.mp3";
-              print("ASSET SETTING ($filename)");
-              player
-                  .setAsset(filename)
-                  .then((Duration duration) => player.play());
+              String filename = "audio/${levelNames[lastLevelPlayed - 1]}.mp3";
+              //print("ASSET SETTING ($filename)");
+              if (!widget.muted)
+                player
+                    .setAsset(filename)
+                    .then((Duration duration) => player.play());
             }
             rawPlayers = buffer.readUint8List(playerCount * 2);
-            rawObjects = buffer.readUint8List(objectCount * 4);
+            rawObjects = buffer.readUint8List(objectCount * 5);
             goal = buffer.readUint8List(2);
             buffer.checkpoint();
-            print("hello...");
+            //print("hello...");
           });
         }
       }
@@ -152,7 +260,7 @@ class _GameScreenState extends State<GameScreen> {
   }
 
   List<int> goal = [20, 20];
-  List<int> size = [0, 0];
+  List<int> size = [1, 1];
   int lastLevelPlayed = -1;
 
   List<int> rawPlayers = [];
@@ -169,9 +277,10 @@ class _GameScreenState extends State<GameScreen> {
   List<List<int>> get objects {
     List<int> poses = rawObjects;
     List<List<int>> result = [];
-    for (int i = 0; i < poses.length; i += 4) {
-      print(poses.sublist(i, i + 4));
-      result.add([poses[i], poses[i + 1], poses[i + 2], poses[i + 3]]);
+    for (int i = 0; i < poses.length; i += 5) {
+      //print(poses.sublist(i, i + 5));
+      result.add(
+          [poses[i], poses[i + 1], poses[i + 2], poses[i + 3], poses[i + 4]]);
     }
     return result;
   }
@@ -185,47 +294,79 @@ class _GameScreenState extends State<GameScreen> {
           FocusNode x,
           RawKeyEvent e,
         ) {
-          print("got $e");
-          switch (e.character) {
-            case "d":
-              server.add([playerIndex, 1]);
-              break;
-            case "a":
-              server.add([playerIndex, 2]);
-              break;
-            case "s":
-              server.add([playerIndex, 3]);
-              break;
-            case "w":
-              server.add([playerIndex, 4]);
-              break;
-            case "+":
-              playerIndex++;
-              server.add([playerIndex, 0]);
-              break;
-            case "-":
-              if (playerIndex > 0) playerIndex--;
-              server.add([playerIndex, 0]);
-              break;
-            default:
-              return false;
-          }
+          //print("got $e");
+          if (!playerMoving) {
+            switch (e.character) {
+              case "d":
+                server.add([playerIndex, 1]);
+                break;
+              case "a":
+                server.add([playerIndex, 2]);
+                break;
+              case "s":
+                server.add([playerIndex, 3]);
+                break;
+              case "w":
+                server.add([playerIndex, 4]);
+                break;
+              case "+":
+                playerIndex++;
+                server.add([playerIndex, 0]);
+                break;
+              case "-":
+                if (playerIndex > 0) playerIndex--;
+                server.add([playerIndex, 0]);
+                break;
+              case "n":
+                server.add([playerIndex, 5]);
+                break;
+              case "r":
+                server.add([playerIndex, 6]);
+                break;
+              default:
+                return false;
+            }
+          } else print("$playerMoving");
           return true;
         },
         child: Scaffold(
           body: Stack(
             children: [
               Container(
-                color: Colors.black,
-                child: CustomPaint(
-                  painter: WorldDrawer(
-                    Size(size[0] / 1, size[1] / 1),
-                    players,
-                    objects,
-                    goal,
-                    _imageInfo,
+                color: Colors.brown,
+                child: TweenAnimationBuilder(
+                  duration: Duration(milliseconds: 500),
+                  child: Container(),
+                  onEnd: () => playerMoving = false,
+                  tween: WorldTween(
+                    WorldState(
+                      Size(size[0] / 1, size[1] / 1),
+                      players,
+                      objects
+                          .map((e1) => e1.map((e2) => e2 / 1).toList())
+                          .toList(),
+                      goal,
+                    ),
+                    WorldState(
+                      Size(size[0] / 1, size[1] / 1),
+                      players,
+                      objects
+                          .map((e1) => e1.map((e2) => e2 / 1).toList())
+                          .toList(),
+                      goal,
+                    ),
                   ),
-                  child: SizedBox.expand(),
+                  builder:
+                      (BuildContext context, WorldState state, Widget widget) {
+                    return CustomPaint(
+                      painter: WorldDrawer(
+                        state,
+                        _buttonImageInfo,
+                        _crateImageInfo,
+                      ),
+                      child: SizedBox.expand(),
+                    );
+                  },
                 ),
               ),
               Center(
@@ -251,25 +392,25 @@ class _GameScreenState extends State<GameScreen> {
 }
 
 class WorldDrawer extends CustomPainter {
-  final List<Offset> players;
-  final List<List<int>> objects;
-  final List<int> goal;
+  final WorldState state;
+  final ImageInfo crateImageInfo;
 
-  final Size size;
+  WorldDrawer(this.state, this.buttonImageInfo, this.crateImageInfo);
 
-  WorldDrawer(
-      this.size, this.players, this.objects, this.goal, this.buttonImageInfo);
-
-  ImageInfo buttonImageInfo;
+  final ImageInfo buttonImageInfo;
 
   @override
   void paint(Canvas canvas, Size totalSize) {
+    Size size = state.size;
+    List<List<double>> objects = state.objects;
+    List<int> goal = state.goal;
+    List<Offset> players = state.players;
     Size worldSize = Size.square(totalSize.shortestSide);
     //print("$worldSize, $size");
     double circleRadius = worldSize.shortestSide / (size.longestSide * 2);
 
     var unitDim = (worldSize.height / size.longestSide);
-    print(worldSize);
+    //print(worldSize);
     if (size.longestSide > size.shortestSide)
       worldSize = size.width > size.height
           ? Size(worldSize.width, unitDim * size.shortestSide)
@@ -277,7 +418,7 @@ class WorldDrawer extends CustomPainter {
     //print("wSize: $worldSize, totSize: $totalSize");
     var topLeft = Offset((totalSize.width - worldSize.width) / 2,
         (totalSize.height - worldSize.height) / 2);
-    print(worldSize);
+    //print(worldSize);
     canvas.drawRect(topLeft & worldSize, Paint()..color = Colors.white70);
     //print(players);
     for (int i = 0; i < objects.length; i++) {
@@ -285,7 +426,7 @@ class WorldDrawer extends CustomPainter {
       drawObject(
           Size.square(circleRadius * 2),
           topLeft + (offset * circleRadius * 2),
-          objects[i][2],
+          objects[i][2].toInt(),
           objects[i][3],
           canvas);
     }
@@ -305,25 +446,40 @@ class WorldDrawer extends CustomPainter {
                 Offset(circleRadius, circleRadius),
             circleRadius - 20,
             Paint()..color = Colors.white);
+      } else {
+        canvas.drawCircle(
+            topLeft +
+                (offset * circleRadius * 2) +
+                Offset(circleRadius, circleRadius),
+            circleRadius - 21,
+            Paint()
+              ..color = Color.fromARGB(
+                  0xFF, r.nextInt(0xFF), r.nextInt(0xFF), r.nextInt(0xFF)));
       }
-      canvas.drawCircle(
-          topLeft +
-              (offset * circleRadius * 2) +
-              Offset(circleRadius, circleRadius),
-          circleRadius - 21,
-          Paint()
-            ..color = Color.fromARGB(
-                0xFF, r.nextInt(0xFF), r.nextInt(0xFF), r.nextInt(0xFF)));
     }
+    Random r = Random(playerIndex);
+    //print("$players * $circleRadius * 2");
+    canvas.drawCircle(
+        topLeft +
+            ((players.length > playerIndex
+                    ? players[playerIndex]
+                    : Offset.zero) *
+                circleRadius *
+                2) +
+            Offset(circleRadius, circleRadius),
+        circleRadius - 21,
+        Paint()
+          ..color = Color.fromARGB(
+              0xFF, r.nextInt(0xFF), r.nextInt(0xFF), r.nextInt(0xFF)));
   }
 
   void drawObject(
-      Size size, Offset pos, int type, int data, Canvas canvas) async {
+      Size size, Offset pos, int type, double data, Canvas canvas) async {
     switch (type) {
       case 0:
         canvas.drawRect(
           pos & size,
-          Paint()..color = Colors.black,
+          Paint()..color = Colors.brown,
         );
         break;
       case 1:
@@ -337,13 +493,41 @@ class WorldDrawer extends CustomPainter {
             filterQuality: FilterQuality.none);
         break;
       case 2:
-        if (data == 0)
-          canvas.drawLine(
-              size.bottomCenter(pos),
-              size.topCenter(pos),
-              Paint()
-                ..color = Colors.brown
-                ..strokeWidth = 10);
+        //print("data: $data");
+        canvas.drawLine(
+          size.topCenter(pos),
+          size.center(pos) - Offset(0, data * size.height / 2),
+          Paint()
+            ..color = Colors.brown[900]
+            ..strokeWidth = 10,
+        );
+        canvas.drawLine(
+          size.bottomCenter(pos),
+          size.center(pos) + Offset(0, data * size.height / 2),
+          Paint()
+            ..color = Colors.brown[900]
+            ..strokeWidth = 10,
+        );
+        break;
+      case 3:
+        if(crateImageInfo == null) {
+          canvas.drawRect(
+          pos & size,
+          Paint()..color = Colors.grey,
+        );
+        }
+        paintImage(
+            canvas: canvas,
+            rect: pos & size,
+            image: crateImageInfo?.image,
+            fit: BoxFit.fill,
+            filterQuality: FilterQuality.none);
+        break;
+      case 4:
+        canvas.drawRect(
+          pos & size,
+          Paint()..color = Colors.black,
+        );
         break;
       default:
         throw "Unsupported '$type' type of object";
